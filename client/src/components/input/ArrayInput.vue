52cc29c0185b1d9a71429213af08a84bacea40ad
jarlion <jarlion@gmail.com>
2025-08-29 01:45:01 +0800

    # 数组输入组件

--- client/src/components/input/ArrayInput.vue
+++ client/src/components/input/ArrayInput.vue
@@ -1,11 +1,18 @@
 <template>
-  <el-input v-model="model" />
+  <el-input v-for="i in index" v-model="model[i]" :key="i"/>
 </template>
-<script lang="ts" generic="T">
-import { defineModel, type PropType } from "vue";
+<script lang="ts" setup generic="T">
+import { computed, defineModel, type PropType } from "vue";
 
 const model = defineModel({
   type: Array as PropType<T[]>,
   default: () => [],
+});
+
+const index = computed(() => {
+  const arr = model.value.map((v, i) => i);
+
+  arr.push(model.value.length);
+  return arr
 });
 </script>
