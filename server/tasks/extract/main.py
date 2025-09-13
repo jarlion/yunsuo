f7c72fb70fa5c737f2cf539324c25cd3d4b0af51
jarlion <jarlion@gmail.com>
2025-09-13 12:02:23 +0800

    + 解压命令

---
+++ server/tasks/extract/main.py
@@ -0,0 +1,81 @@
+import zipfile
+from typing import List, Dict, Any
+from pathlib import Path
+import rarfile
+import py7zr
+
+def main(params:Dict[str, Any], ctx:Dict[str, Any]) -> Any|None:
+    source_str = params.get('source')
+    if not source_str:
+        raise ValueError("Missing required params: source")
+    source = source_str.strip()
+    if source_str.startswith('>'):
+        source = eval(source_str[1:], globals(), ctx)
+    result = extract(source)
+    if result:
+        return [result]
+    return None
+
+def try_unzip(path:Path):
+    try:
+        with zipfile.ZipFile(path, 'r') as zip_ref:
+            zip_ref.extractall(path.parent)
+        path = path.parent / path.stem
+    except zipfile.BadZipFile as e:
+        print(e)
+        return None
+    return path
+
+def extract(source:str) -> Path|None:
+    source_path = Path(source)
+    # 解压zip
+    result = try_unzip(source_path)
+    if result:
+        return result
+    # 解压rar
+    result = try_unrar(source_path)
+    if result:
+        return result
+    # 解压7z
+    result = try_un7z(source_path)
+    if result:
+        return result
+    return None
+
+def try_unrar(path:Path) -> Path|None:
+    try:
+        with rarfile.RarFile(path, 'r') as rar_ref:
+            rar_ref.extractall(path.parent)
+        path = path.parent / path.stem
+    except Exception as e:
+        print(e)
+        return None
+    return path
+
+def try_un7z(path:Path) -> Path|None:
+    try:
+        with py7zr.SevenZipFile(path, 'r') as z:
+            z.extractall(path.parent)
+        path = path.parent / path.stem
+    except py7zr.Bad7zFile as e:
+        print(e)
+        return None
+    return path
+
+if __name__ == '__main__':
+    def exec_script(script:str, ctx:Dict[str, Any], env:Dict[str, Any]|None=None)->Any:
+        if env is None:
+            env = globals()
+        locals = ctx.copy()
+        locals['output'] = None
+        exec(script, env, locals)
+        return locals.get('output', None)
+
+    result = ["v:\\v20250825100235\\六花 喝醉 战斗衔接  锤 击中踩点（邪王真眼） S1 .mp4"]
+    params = {
+        "before": "output=cd=result",
+        "condition": ">len(cd)",
+        "after": "output=cd[:]=cd[1:]"
+    }
+    dirs = main(params, {"exec_script": exec_script, "result": result})
+    print(dirs)
