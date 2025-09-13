b8c99842761973f9ba34aaa557253eb381102fdd
jarlion <jarlion@gmail.com>
2025-09-13 12:58:39 +0800

    + 解压密码

--- server/tasks/extract/main.py
+++ server/tasks/extract/main.py
@@ -8,54 +8,63 @@ def main(params:Dict[str, Any], ctx:Dict[str, Any]) -> Any|None:
     source_str = params.get('source')
     if not source_str:
         raise ValueError("Missing required params: source")
+    password = params.get('password')
+    if not password:
+        raise ValueError("Missing required params: password")
+    type = params.get('type')
+    if not type:
+        raise ValueError("Missing required params: type")
+    if type not in ['zip', 'rar', '7z']:
+        raise ValueError("Invalid params: type")
     source = source_str.strip()
     if source_str.startswith('>'):
         source = eval(source_str[1:], globals(), ctx)
-    result = extract(source)
+    result = extract(source, password, type)
     if result:
         return [result]
     return None
 
-def try_unzip(path:Path):
+def extract(source:str, password:str, type:str) -> Path|None:
+    source_path = Path(source)
+    # 解压zip
+    result = try_unzip(source_path, password)
+    if result:
+        return result
+    # 解压rar
+    result = try_unrar(source_path, password)
+    if result:
+        return result
+    # 解压7z
+    result = try_un7z(source_path, password)
+    if result:
+        return result
+    return None
+
+
+def try_unzip(path:Path, password:str) -> Path|None:
     try:
         with zipfile.ZipFile(path, 'r') as zip_ref:
-            zip_ref.extractall(path.parent)
+            zip_ref.extractall(path.parent, pwd=password.encode() if password else None)
         path = path.parent / path.stem
     except zipfile.BadZipFile as e:
         print(e)
         return None
     return path
 
-def extract(source:str) -> Path|None:
-    source_path = Path(source)
-    # 解压zip
-    result = try_unzip(source_path)
-    if result:
-        return result
-    # 解压rar
-    result = try_unrar(source_path)
-    if result:
-        return result
-    # 解压7z
-    result = try_un7z(source_path)
-    if result:
-        return result
-    return None
-
-def try_unrar(path:Path) -> Path|None:
+def try_unrar(path:Path, password:str) -> Path|None:
     try:
         with rarfile.RarFile(path, 'r') as rar_ref:
-            rar_ref.extractall(path.parent)
+            rar_ref.extractall(path.parent, pwd=password)
         path = path.parent / path.stem
     except Exception as e:
         print(e)
         return None
     return path
 
-def try_un7z(path:Path) -> Path|None:
+def try_un7z(path:Path, password:str) -> Path|None:
     try:
         with py7zr.SevenZipFile(path, 'r') as z:
-            z.extractall(path.parent)
+            z.extractall(path.parent, password=password.encode() if password else None)
         path = path.parent / path.stem
     except py7zr.Bad7zFile as e:
         print(e)
